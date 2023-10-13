77e98a2034478a0650d5cd447153f78b9c0b7939
Yoshiki Okesaku <yoshiki.o1994@gmail.com>
2023-10-13 11:49:53 +0900

    add pan camera

--- src/bin/osc2r2.rs
+++ src/bin/osc2r2.rs
@@ -1,6 +1,7 @@
 use osc2r2;
 
 use bevy::{
+    input::mouse::{MouseMotion, MouseWheel},
     pbr::wireframe::{Wireframe, WireframeConfig, WireframePlugin},
     prelude::*,
     render::render_resource::{PrimitiveTopology, WgpuFeatures},
@@ -21,8 +22,120 @@ fn main() {
             WireframePlugin,
         ))
         .add_systems(Startup, setup)
-        .add_systems(Update, update_camera_transform)
+        .add_systems(Update, pan_orbit_camera)
         .run();
+}
+
+#[derive(Component)]
+struct PanOrbitCamera {
+    pub focus: Vec3,
+    pub radius: f32,
+    pub upside_down: bool,
+}
+
+impl Default for PanOrbitCamera {
+    fn default() -> Self {
+        Self {
+            focus: Vec3::ZERO,
+            radius: 5.0,
+            upside_down: false,
+        }
+    }
+}
+
+fn pan_orbit_camera(
+    windows: Query<&Window>,
+    input_mouse: Res<Input<MouseButton>>,
+    mut ev_motion: EventReader<MouseMotion>,
+    mut ev_scroll: EventReader<MouseWheel>,
+    mut query: Query<(&mut PanOrbitCamera, &mut Transform, &Projection), With<Camera>>,
+) {
+    let orbit_button = MouseButton::Right;
+    let pan_button = MouseButton::Left;
+
+    let mut pan = Vec2::ZERO;
+    let mut rotation_move = Vec2::ZERO;
+    let mut scroll = 0.0;
+    let mut orbit_button_changed = false;
+
+    if input_mouse.pressed(orbit_button) {
+        for ev in ev_motion.iter() {
+            rotation_move += ev.delta / 2.0;
+        }
+    } else if input_mouse.pressed(pan_button) {
+        for ev in ev_motion.iter() {
+            pan += ev.delta / 2.0;
+        }
+    }
+
+    for ev in ev_scroll.iter() {
+        scroll += ev.y / 10.0;
+    }
+
+    if input_mouse.just_released(orbit_button) || input_mouse.just_pressed(orbit_button) {
+        orbit_button_changed = true;
+    }
+
+    for (mut pan_orbit, mut transform, projection) in query.iter_mut() {
+        if orbit_button_changed {
+            // only check for upside down when orbiting started or ended this frame
+            // if the camera is "upside" down, panning horizontally would be inverted, so invert the input to make it correct
+            let up = transform.rotation * Vec3::Y;
+            pan_orbit.upside_down = up.y <= 0.0;
+        }
+
+        let mut any = false;
+        if rotation_move.length_squared() > 0.0 {
+            any = true;
+            let window = get_primary_window_size(windows.single());
+            let delta_x = {
+                let delta = rotation_move.x / window.x * std::f32::consts::PI * 2.0;
+                if pan_orbit.upside_down {
+                    -delta
+                } else {
+                    delta
+                }
+            };
+            let delta_y = rotation_move.y / window.y * std::f32::consts::PI;
+            let pitch = Quat::from_rotation_x(-delta_x);
+            let yaw = Quat::from_rotation_y(-delta_y);
+            transform.rotation = yaw * transform.rotation; // rotate around global y axis
+            transform.rotation = pitch * transform.rotation; // rotate around global x axis
+        } else if pan.length_squared() > 0.0 {
+            any = true;
+            // make panning distance independent of resolution and FOV,
+            let window = get_primary_window_size(windows.single());
+            if let Projection::Perspective(projection) = projection {
+                pan *= Vec2::new(projection.fov * projection.aspect_ratio, projection.fov) / window;
+            }
+            // translate by local axes
+            let right = transform.rotation * Vec3::X * -pan.x;
+            let up = transform.rotation * Vec3::Y * pan.y;
+            // make panning proportional to distance away from focus point
+            let translation = (right + up) * pan_orbit.radius;
+            pan_orbit.focus += translation;
+        } else if scroll.abs() > 0.0 {
+            any = true;
+            pan_orbit.radius -= scroll * pan_orbit.radius * 0.2;
+            // dont allow zoom to reach zero or you get stuck
+            pan_orbit.radius = f32::max(pan_orbit.radius, 0.05);
+        }
+        if any {
+            // emulating parent/child to make the yaw/y-axis rotation behave like a turntable
+            // parent = x and y rotation
+            // child = z-offset
+            let rot_matrix = Mat3::from_quat(transform.rotation);
+            transform.translation =
+                pan_orbit.focus + rot_matrix.mul_vec3(Vec3::new(0.0, 0.0, pan_orbit.radius));
+        }
+    }
+    // consume any remaining events, so they don't pile up if we don't need them
+    // (and also to avoid Bevy warning us about not checking events every frame update)
+    ev_motion.clear();
+}
+
+fn get_primary_window_size(window: &Window) -> Vec2 {
+    Vec2::new(window.width(), window.height())
 }
 
 fn setup(
@@ -57,14 +170,23 @@ fn setup(
         }
     }
 
-    commands.spawn(Camera3dBundle {
-        camera: Camera {
-            hdr: true, // 1. HDR is required for bloom
+    let translation = Vec3::new(500.0, 25.0, 50.0);
+    let radius = translation.length();
+
+    commands.spawn((
+        Camera3dBundle {
+            camera: Camera {
+                hdr: true, // 1. HDR is required for bloom
+                ..default()
+            },
+            transform: Transform::from_translation(translation).looking_at(Vec3::ZERO, Vec3::Z),
             ..default()
         },
-        transform: Transform::from_xyz(500.0, 25.0, 50.0).looking_at(Vec3::ZERO, Vec3::Z),
-        ..default()
-    });
+        PanOrbitCamera {
+            radius: radius,
+            ..default()
+        },
+    ));
 
     let mut mesh = Mesh::new(PrimitiveTopology::TriangleList);
 
@@ -139,50 +261,4 @@ fn setup(
     }
 
     commands.spawn(DirectionalLightBundle::default());
-}
-
-fn update_camera_transform(
-    mut camera_query: Query<&mut Transform, With<Camera>>,
-    keycode: Res<Input<KeyCode>>,
-) {
-    let mut camera_transform = camera_query.single_mut();
-
-    let mut move_forward = 0.0f32;
-    let mut turn_left = 0.0f32;
-    let mut turn_up = 0.0f32;
-    const MOVE_UNIT: f32 = 5.0;
-    const TURN_UNIT: f32 = 0.01;
-    if keycode.pressed(KeyCode::Up) {
-        move_forward += MOVE_UNIT;
-    }
-    if keycode.pressed(KeyCode::Down) {
-        move_forward -= MOVE_UNIT;
-    }
-    if keycode.pressed(KeyCode::D) {
-        turn_left -= TURN_UNIT;
-    }
-    if keycode.pressed(KeyCode::A) {
-        turn_left += TURN_UNIT;
-    }
-    if keycode.pressed(KeyCode::W) {
-        turn_up += TURN_UNIT;
-    }
-    if keycode.pressed(KeyCode::S) {
-        turn_up -= TURN_UNIT;
-    }
-
-    if move_forward != 0.0 {
-        let translation = camera_transform.forward() * move_forward;
-        camera_transform.translation += translation;
-    }
-
-    if turn_left != 0.0 {
-        let rotation = Quat::from_rotation_z(turn_left);
-        camera_transform.rotate(rotation);
-    }
-
-    if turn_up != 0.0 {
-        let rotation = Quat::from_rotation_y(turn_up);
-        camera_transform.rotate(rotation);
-    }
 }
